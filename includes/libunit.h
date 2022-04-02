4a1dd9645d45fe5a5477e66c4317b336b06bfa27
Chafik Taleb <taleb.cas@gmail.com>
2022-04-02 16:09:59 +0200

    normed files; added failure check for fork function; status check should now be properly done

--- includes/libunit.h
+++ includes/libunit.h
@@ -12,11 +12,21 @@
 
 typedef struct s_unit_test
 {
-	char *test_name;
-	int (*f)(void);
-	void *next;
+	char	*test_name;
+	int		(*f)(void);
+	void	*next;
 }	t_unit_test;
 
+typedef struct s_test_data
+{
+	int		test_count;
+	int		test_passed;
+	pid_t	pid;
+}	t_test_data;
+
 void	launch_tests(t_unit_test *testlist);
+void	print_test(t_unit_test *testlist, int *test_count);
+void	print_test_status(t_unit_test *tlist, int t_count, int *t_passed, int status)
+void	print_result(int test_count, int test_passed);
 
 #endif
---
+++ src/launch_tests_utils.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   launch_tests_utils.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctaleb <ctaleb@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 15:14:21 by ctaleb            #+#    #+#             */
+/*   Updated: 2022/04/02 15:47:35 by ctaleb           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/libunit.h"
+#include "../libft/libft.h"
+
+void	print_test(t_unit_test *testlist, int *test_count)
+{
+	test_count++;
+	ft_putstr_fd("Test ", 1);
+	ft_putnbr_fd(test_count, 1);
+	ft_putstr_fd(":", 1);
+	ft_putstr_fd(testlist->test_name, 1);
+	ft_putstr_fd(":", 1);
+}
+
+void	print_test_status(int *test_passed, int status)
+{
+	if (status == 0)
+	{
+		(*test_passed)++;
+		ft_putstr_fd("\033[32m[OK]\n\033[0m", 1);
+	}
+	else if (status == SIGSEGV)
+		ft_putstr_fd("\033[33m[SIGSEGV]\n\033[0m", 1);
+	else if (status == SIGBUS)
+		ft_putstr_fd("\033[33m[SIGBUS]\n\033[0m", 1);
+	else if (status == SIGABRT)
+		ft_putstr_fd("\033[33m[SIGABRT]\n\033[0m", 1);
+	else if (status == SIGFPE)
+		ft_putstr_fd("\033[33m[SIGFPE]\n\033[0m", 1);
+	else if (status == SIGPIPE)
+		ft_putstr_fd("\033[33m[SIGPIPE]\n\033[0m", 1);
+	else if (status == SIGILL)
+		ft_putstr_fd("\033[33m[SIGILL]\n\033[0m", 1);
+	else if (status == -1)
+		ft_putstr_fd("\033[1;30m[TIMEOUT]\n\033[0m", 1);
+	else
+		ft_putstr_fd("\033[31m[KO]\n\033[0m", 1);
+}
+
+void	print_result(int test_count, int test_passed)
+{
+	ft_putstr_fd("\n", 1);
+	ft_putnbr_fd(test_passed, 1);
+	ft_putstr_fd("/", 1);
+	ft_putnbr_fd(test_count, 1);
+	ft_putstr_fd(" tests checked\n", 1);
+}
