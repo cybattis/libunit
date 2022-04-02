9a7c9e1361a53984e9d89be39c91fadb2d7852db
Cyril Battistolo <cybattis@student.42lyon.fr>
2022-04-02 15:47:18 +0200

    revert launch_tests.c after merge

--- src/launch_tests.c
+++ src/launch_tests.c
@@ -1,8 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   launch_tests.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctaleb <ctaleb@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 14:52:29 by ctaleb            #+#    #+#             */
+/*   Updated: 2022/04/02 14:52:36 by ctaleb           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/libunit.h"
 #include "../libft/libft.h"
 
 static void	print_test(t_unit_test *testlist, int test_count, int *test_passed, int status);
 static void	print_result(int test_count, int test_passed);
+
+void	timeout_handler(int sig)
+{
+	(void)sig;
+	exit(-1);
+}
 
 void	launch_tests(t_unit_test *testlist)
 {
@@ -18,6 +36,9 @@ void	launch_tests(t_unit_test *testlist)
 		pid = fork();
 		if (pid == 0)
 		{
+			signal(SIGALRM, timeout_handler);
+			if (TIMEOUT > 0)
+				alarm(TIMEOUT);
 			testlist->f();
 			exit(0);
 		}
@@ -26,17 +47,16 @@ void	launch_tests(t_unit_test *testlist)
 			wait(&pid);
 			if (WEXITSTATUS(pid) == 0)
 				print_test(testlist, test_count, &test_passed, 0);
-			else if (WSTOPSIG(pid) == SIGBUS)
-				print_test(testlist, test_count, &test_passed, SIGBUS);
-			else if (WSTOPSIG(pid) == SIGSEGV)
-				print_test(testlist, test_count, &test_passed, SIGSEGV);
-			else
+			else if (WEXITSTATUS(pid) == -1)
 				print_test(testlist, test_count, &test_passed, -1);
+			else if (WIFSTOPPED(pid))
+				print_test(testlist, test_count, &test_passed, WSTOPSIG(pid));
+			else
+				print_test(testlist, test_count, &test_passed, -2);
 		}
 		testlist = testlist->next;
 	}
 	print_result(test_count, test_passed);
-	ft_lstclear(&testlist, free);
 }
 
 static void	print_test(t_unit_test *testlist, int test_count, int *test_passed, int status)
@@ -49,14 +69,24 @@ static void	print_test(t_unit_test *testlist, int test_count, int *test_passed, 
 	if (status == 0)
 	{
 		(*test_passed)++;
-		ft_putstr_fd("[OK]\n", 1);
+		ft_putstr_fd("\033[32mOK\n\033[0m", 1);
 	}
 	else if (status == SIGSEGV)
-		ft_putstr_fd("SIGSEGV\n", 1);
+		ft_putstr_fd("\033[33mSIGSEGV\n\033[0m", 1);
 	else if (status == SIGBUS)
-		ft_putstr_fd("SIGBUS\n", 1);
+		ft_putstr_fd("\033[33mSIGBUS\n\033[0m", 1);
+	else if (status == SIGABRT)
+		ft_putstr_fd("\033[33mSIGABRT\n\033[0m", 1);
+	else if (status == SIGFPE)
+		ft_putstr_fd("\033[33mSIGFPE\n\033[0m", 1);
+	else if (status == SIGPIPE)
+		ft_putstr_fd("\033[33mSIGPIPE\n\033[0m", 1);
+	else if (status == SIGILL)
+		ft_putstr_fd("\033[33mSIGILL\n\033[0m", 1);
+	else if (status == -1)
+		ft_putstr_fd("\033[1;30mTIMEOUT\n\033[0m", 1);
 	else
-		ft_putstr_fd("Unknown error\n", 1);
+		ft_putstr_fd("\033[31mKO\n\033[0m", 1);
 }
 
 static void	print_result(int test_count, int test_passed)
